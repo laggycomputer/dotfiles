eec88c614d87fdc3508d97327650ab1b147ef6a3
Dante Dam <laggycomputer@yahoo.com>
2025-03-21 09:44:54 -0700

    fix mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 return {
     {
         "williamboman/mason.nvim",
-        setup = function()
+        config = function()
             -- Mason Setup
             require("mason").setup({
                 ui = {
