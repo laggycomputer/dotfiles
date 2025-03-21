a9ab810a64e62bb14f49250aace4e07774d961d4
Dante Dam <laggycomputer@yahoo.com>
2025-03-21 09:27:06 -0700

    dep checks for mason and lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,7 @@
 return {
     {
         "neovim/nvim-lspconfig",
+        dependencies = { "mason-lspconfig.nvim" },
         config = function()
             -- Set up lspconfig.
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -16,6 +16,7 @@ return {
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        dependencies = { "mason.nvim" },
         setup = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
