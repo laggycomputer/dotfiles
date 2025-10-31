8f89e2394e984a39ee09579183d6f303c0587ea8
Dante Dam <laggycomputer@yahoo.com>
2025-10-31 14:42:02 -0700

    init zoxide in shell_startup.sh

--- shell_startup.sh
+++ shell_startup.sh
@@ -2,6 +2,8 @@ alias pls="sudo"
 alias up="cd ../"
 alias sl="sl -e"
 
+eval "$(zoxide init bash)"
 eval "$(oh-my-posh init bash --config ~/.omp.json)"
 export EDITOR=nvim
 fortune -s | chara say -c ferris
+
