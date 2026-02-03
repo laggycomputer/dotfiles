2c5d61b2c3f1945bc1f9aa4017bacd3248a1e40f
Dante Dam <laggycomputer@yahoo.com>
2026-02-03 13:50:06 -0800

    gdbinit

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 echo "export DOTFILES_DIR=$(pwd)" >> ~/.bashrc
 echo "source \${DOTFILES_DIR}/shell_startup.sh" >> ~/.bashrc
 ln -s ${PWD}/.omp.json ~/.omp.json
+mkdir -p ~/.config/gdb
+ln -s ${PWD}/gdbinit ~/.config/gdb/gdbinit
