8d0b1bcc47be14cc0f87cfeef3ca70e1c3735925
Dante Dam <laggycomputer@yahoo.com>
2026-02-09 15:54:00 -0800

    give in and migrate

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -6,10 +6,9 @@ return {
         config = function()
             -- Set up lspconfig.
             local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-            -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 
-            local lspconfig = require('lspconfig')
-            --
+            local lspconfig = vim.lsp.config
+
             lspconfig['lua_ls'].setup {
                 capabilities = capabilities,
                 Lua = {
