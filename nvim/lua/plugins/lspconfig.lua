a19cbbb5f28323c2289e186523ba6e4db6221ad5
Dante Dam <laggycomputer@yahoo.com>
2026-02-09 16:18:42 -0800

    enable these?

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,21 +4,19 @@ return {
         dependencies = { "mason-lspconfig.nvim", "hrsh7th/cmp-nvim-lsp" },
         event = "BufReadPre",
         config = function()
-            -- Set up lspconfig.
             local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-            local lspconfig = vim.lsp.config
-
-            lspconfig['lua_ls'].setup {
+            vim.lsp.config("lua_ls", {
                 capabilities = capabilities,
                 Lua = {
                     hint = {
                         enable = true,
                     }
                 },
-            }
+            })
+            vim.lsp.enable("lua_ls")
 
-            lspconfig['rust_analyzer'].setup{
+            vim.lsp.config("rust_analyzer", {
                 capabilities = capabilities,
                 on_attach = function(client, bufnr)
                     if client.server_capabilities.inlayHintProvider then
@@ -67,9 +65,10 @@ return {
                         },
                     }
                 }
-            }
+            })
+            vim.lsp.enable("rust_analyzer")
 
-            lspconfig.pylyzer.setup({
+            vim.lsp.config("pylyzer", {
                 capabilities = capabilities,
                 settings = {
                     python = {
@@ -88,29 +87,34 @@ return {
                     end
                 end,
             })
+            vim.lsp.enable("pylyzer")
 
-            lspconfig["tinymist"].setup {
+            vim.lsp.config("tinymist", {
                 capabilities = capabilities,
                 settings = {
                     formatterMode = "typstyle",
                     exportPdf = "onType",
                     semanticTokens = "disable"
                 }
-            }
+            })
+            vim.lsp.enable("tinymist")
 
-            lspconfig.clangd.setup {
-                capabilities = capabilities,
-            }
-
-            lspconfig.ts_ls.setup {
-                filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-                cmd = { "typescript-language-server", "--stdio" }
-            }
-
-            -- npm i -g @astrojs/language-server
-            lspconfig['astro'].setup({
+            vim.lsp.config("clangd", {
                 capabilities = capabilities,
             })
+            vim.lsp.enable("clangd")
+
+            vim.lsp.config("ts_ls", {
+                filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+                cmd = { "typescript-language-server", "--stdio" }
+            })
+            vim.lsp.enable("ts_ls")
+
+            -- npm i -g @astrojs/language-server
+            vim.lsp.config("astro", {
+                capabilities = capabilities,
+            })
+            vim.lsp.enable("astro")
         end
     }
 }
